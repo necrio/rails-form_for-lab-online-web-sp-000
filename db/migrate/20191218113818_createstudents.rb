57a839d03d3c568973444c44048ec44241692b14
Keith Isham <kisham8899@gmail.com>
2019-12-18 11:39:57 +0000

    Automatically backed up by Learn

--- db/migrate/20191218113818_createstudents.rb
+++ db/migrate/20191218113818_createstudents.rb
@@ -1,4 +1,9 @@
 class Createstudents < ActiveRecord::Migration[5.0]
   def change
+    create_table :students do |t|
+      t.string :first_name
+      t.string :last_name
+      
+      t.timestamps null: false
   end
 end
